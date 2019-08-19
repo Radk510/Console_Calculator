3e3801ab64e3804350167f46774167f57401a4c0
radk510 <radk510@yandex.ru>
2019-08-19 16:26:18 +0300

    docs: create class with method to add values

---
+++ Console_Calculator/Console_Calculator/GetValues.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Calculator
+{
+    class GetValues
+    {       
+        public double GetValuesToCount()
+        {
+            double number = 0;
+            bool propperNumber = double.TryParse(Console.ReadLine(), out number); ;
+            while (!propperNumber)
+            {
+                Console.Write("Try again. You should type only the number: ");
+                propperNumber = double.TryParse(Console.ReadLine(), out number);
+            }
+            return number;
+        }
+
+    }
+}
