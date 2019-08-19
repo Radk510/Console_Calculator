5eee19bd4fcd837dc4635c4cdb1025ad5cc49977
radk510 <radk510@yandex.ru>
2019-08-19 14:53:07 +0300

    docs: create files and write simple code

---
+++ Console_Calculator/Console_Calculator/Math_Operations.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Calculator
+{
+    public static class Math_Operations
+    {       
+        public static void Operations(double numberOne, char mathOperator, double numberTwo)
+        {
+            switch (mathOperator)
+            {
+                case '+':
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("The answer is: " + (numberOne + numberTwo));
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    break;
+                case '-':
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("The answer is: " + (numberOne - numberTwo));
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    break;
+                case '*':
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("The answer is: " + (numberOne * numberTwo));
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    break;
+                case '/':
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("The answer is: " + (numberOne / numberTwo));
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("Type the propper sign to make the calculation");
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    break;
+            }
+        }
+
+
+
+    }
+}
---
+++ Console_Calculator/Console_Calculator/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char continueOrNot = 'y';
+            do
+            {
+                Console.WriteLine("This is console Calculator. Want to count something?\n\n");
+            
+                Console.Write("Type the first number: ");
+                double numberOne = 0;
+                bool propperNumberOne = double.TryParse(Console.ReadLine(), out numberOne); ;
+                while (!propperNumberOne)
+                {
+                    Console.Write("Try again. You should type only the number: ");
+                    propperNumberOne = double.TryParse(Console.ReadLine(), out numberOne);
+                    //Console.Clear();
+                }
+
+                Console.Write("Choose what you want to do. Type some of this: '+'; '-'; '*'; '/':  ");
+                char mathOperator;
+                bool propperMathOperator = char.TryParse(Console.ReadLine(), out mathOperator);
+                while (!propperMathOperator)
+                {
+                    Console.Write("Try again. You should type only the math operator: ");
+                    propperMathOperator = char.TryParse(Console.ReadLine(), out mathOperator);
+                }
+
+                Console.Write("Type the second number: ");
+                double numberTwo = 0;
+                bool propperNumberTwo = double.TryParse(Console.ReadLine(), out numberTwo);
+                while (!propperNumberTwo)
+                {
+                    Console.Write("Try again. You should type only the number: ");
+                    propperNumberTwo = double.TryParse(Console.ReadLine(), out numberTwo);
+                }
+
+                Math_Operations.Operations(numberOne, mathOperator, numberTwo);
+
+                Console.WriteLine("Would you like to count something else? Type 'y' if YES, and 'n' if NO: ");
+                bool propperDesition = char.TryParse(Console.ReadLine(), out continueOrNot);
+                while (!propperDesition)
+                {
+                    Console.WriteLine("Try again, you should type 'y' (continue) or 'n' (break)");
+                    propperDesition = char.TryParse(Console.ReadLine(), out continueOrNot);
+                }
+
+            } while (continueOrNot == 'y');
+
+
+            //Console.ReadLine();
+        }
+    }
+}
